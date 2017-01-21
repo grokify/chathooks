d0fe715ebc6f2ed77cef9681935249ef74799f1b
John Wang <johncwang@gmail.com>
2017-01-22 05:17:29 +0800

    add tests

---
+++ server_test.go
@@ -0,0 +1,23 @@
+package glipwebhookproxy
+
+import (
+	"testing"
+)
+
+var ConfigurationTests = []struct {
+	v    int
+	want string
+}{
+	{8080, ":8080"}}
+
+func TestConfiguration(t *testing.T) {
+	for _, tt := range ConfigurationTests {
+		config := Configuration{
+			Port: tt.v}
+
+		addr := config.Address()
+		if tt.want != addr {
+			t.Errorf("Configuration.Address(%v): want %v, got %v", tt.v, tt.want, addr)
+		}
+	}
+}
