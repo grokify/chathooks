887341d5b644e1d81c1b7967382ee0e9814325ad
John Wang <johncwang@gmail.com>
2017-01-22 06:06:50 +0800

    update tests

--- server_test.go
+++ server_test.go
@@ -21,26 +21,3 @@ func TestConfigurationAddress(t *testing.T) {
 		}
 	}
 }
-
-var ConfigurationEmojiTests = []struct {
-	test1 string
-	true1 string
-	test2 string
-	true2 string
-}{
-	{"https://grokify.gitub.io/", "https://grokify.gitub.io/", ".png", ".png"}}
-
-func TestConfigurationEmoji(t *testing.T) {
-	for _, tt := range ConfigurationEmojiTests {
-		config := Configuration{
-			EmojiURLPrefix: tt.test1,
-			EmojiURLSuffix: tt.test1}
-
-		if tt.true1 != config.EmojiURLPrefix {
-			t.Errorf("Configuration.EmojiURLPrefix.%v: want %v, got %v", tt.test1, tt.true1, config.EmojiURLPrefix)
-		}
-		if tt.true1 != config.EmojiURLSuffix {
-			t.Errorf("Configuration.EmojiURLSuffix.%v: want %v, got %v", tt.test2, tt.true2, config.EmojiURLSuffix)
-		}
-	}
-}
