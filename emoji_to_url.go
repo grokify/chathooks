2e9332d4dfa373e6662c7b5d998fca27344eb135
John Wang <johncwang@gmail.com>
2017-01-23 04:38:40 +0800

    update style

--- emoji_to_url.go
+++ emoji_to_url.go
@@ -10,14 +10,13 @@ import (
 // EmojiToURL function takes a `Sprintf` `format` string and emoji string with
 // or without surrounding colons (`:`) and returns a URL. Emoji strings must
 // satisfy `[a-z_]+` regexp.
-func EmojiToURL(format string, emoji string) (string, error) {
+func EmojiToURL(urlFormat string, emoji string) (string, error) {
 	emoji = strings.TrimSpace(emoji)
 	if len(emoji) > 0 {
 		rx := regexp.MustCompile(`^:?([a-z_]+):?$`)
 		rs := rx.FindStringSubmatch(emoji)
 		if len(rs) > 1 {
-			url := fmt.Sprintf(format, rs[1])
-			return url, nil
+			return fmt.Sprintf(urlFormat, rs[1]), nil
 		}
 	}
 	return "", errors.New("No Emoji")
