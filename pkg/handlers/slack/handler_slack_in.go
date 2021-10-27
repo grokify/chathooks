066f94c85535d1713e0bf62bc0a2aedd47c7af83
John Wang <johncwang@gmail.com>
2021-10-27 04:27:09 -0700

    cleanup: handlers/slack: remove unneeded code

--- pkg/handlers/slack/handler_slack_in.go
+++ pkg/handlers/slack/handler_slack_in.go
@@ -23,42 +23,6 @@ func NewHandler() handlers.Handler {
 	return handlers.Handler{MessageBodyType: MessageBodyType, Normalize: Normalize}
 }
 
-/*
-// FastHttp request handler constructor for Slack inbound webhook
-type Handler struct {
-	Config  config.Configuration
-	Adapter adapters.Adapter
-}
-
-// FastHttp request handler constructor for Slack in bound webhook
-func NewHandler(config config.Configuration, adapter adapters.Adapter) Handler {
-	return Handler{Config: config, Adapter: adapter}
-}
-
-func (h Handler) HandlerKey() string {
-	return HandlerKey
-}
-
-func (h Handler) MessageDirection() string {
-	return MessageDirection
-}
-
-// HandleFastHTTP is the method to respond to a fasthttp request.
-func (h Handler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
-	ccMsg, err := Normalize(h.Config, BuildInboundMessageBytes(ctx))
-	if err != nil {
-		ctx.SetStatusCode(fasthttp.StatusNotAcceptable)
-		log.WithFields(log.Fields{
-			"type":   "http.response",
-			"status": fasthttp.StatusNotAcceptable,
-		}).Info(fmt.Sprintf("%v request is not acceptable.", DisplayName))
-		return
-	}
-
-	util.SendWebhook(ctx, h.Adapter, ccMsg)
-}
-*/
-
 func BuildInboundMessageBytes(ctx *fasthttp.RequestCtx) []byte {
 	ct := string(ctx.Request.Header.Peek("Content-Type"))
 	ct = strings.TrimSpace(strings.ToLower(ct))
@@ -77,26 +41,4 @@ func Normalize(config config.Configuration, hReq handlers.HandlerRequest) (cc.Me
 	ccMsg := ccslack.WebhookInBodySlackToCc(slMsg)
 
 	return ccMsg, nil
-	/*
-		message := cc.Message{
-			Activity:  slack.Username,
-			Text:      slack.Text,
-			IconEmoji: slack.IconEmoji,
-			IconURL:   slack.IconURL}
-		return message, nil*/
 }
-
-/*
-type SlackWebhookMessage struct {
-	Username  string `json:"username"`
-	IconEmoji string `json:"icon_emoji"`
-	IconURL   string `json:"icon_url"`
-	Text      string `json:"text"`
-}
-
-func SlackWebhookMessageFromBytes(bytes []byte) (SlackWebhookMessage, error) {
-	msg := webhook.SlackWebhookMessage{}
-	err := json.Unmarshal(bytes, &msg)
-	return msg, err
-}
-*/
